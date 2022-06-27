7d3487b3aedf4bd303f438900000d90fa066a5aa
Chandan Kumar <krc6893@gmail.com>
2022-06-27 17:53:43 +0530

    output

---
+++ JSassignment/assignment.js
@@ -0,0 +1,34 @@
+// / Print all the numbers in the given range.
+function numbers(a,b){
+    while((a+1)<b){
+        
+        
+        
+        a++;
+        console.log(a);
+        
+       
+    }
+//  return numbers(a+1,b);
+}
+
+console.log(numbers(1,10));
+
+
+
+
+
+
+
+// Print all the numbers in descending order in the given range
+// function descending(a,b){
+//     while(b!=0){
+        
+//         b--
+//         console.log(b);
+
+//     }
+    
+// }
+// console.log(descending(1,10));
+
