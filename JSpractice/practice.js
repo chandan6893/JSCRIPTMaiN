d078681951982c12b87c1e4fd6f97ee473995291
Chandan Kumar <krc6893@gmail.com>
2022-06-28 18:28:42 +0530

    practice

---
+++ JSpractice/practice.js
@@ -0,0 +1,26 @@
+// function neww(b,c){
+//     let a=b=c;
+//     // console.log(a);
+//     return (a,a);
+
+// }
+// // neww("Kumar");   
+// console.log(neww("Kumar","Kumbha"));
+
+
+
+
+function neww(b){
+return second(b);
+
+}
+console.log(neww("54"));
+
+function second(z){
+    console.log(z,"SECOND FUCTION CONSOLE");
+    return z;
+}
+
+
+
+
