2f55fddcab15e05f0f4a0d65e950ca4fc1caf686
Chandan Kumar <krc6893@gmail.com>
2022-06-29 19:21:03 +0530

    PATTERN PRACTICE

--- JSpractice/practice.js
+++ JSpractice/practice.js
@@ -10,17 +10,62 @@
 
 
 
-function neww(b){
-return second(b);
+// function neww(b){
+// return second(b);
 
-}
-console.log(neww("54"));
+// }
+// console.log(neww("54"));
 
-function second(z){
-    console.log(z,"SECOND FUCTION CONSOLE");
-    return z;
-}
-
-
+// function second(z){
+//     console.log(z,"SECOND FUCTION CONSOLE");
+//     return z;
+// }
 
 
+// let fname=10
+// console.log(fname);
+
+
+
+
+// 
+// let num=5;
+// for(let i=1;i<=num;i++){
+//     let stars =" ";
+//     for(let j=1;j<=num;j++){
+//         stars=stars+"*";
+//         // console.log(stars);
+//     }
+//     console.log(stars);
+// }
+
+// let num=5;
+// for(let i=1;i<=num;i++){
+//     for(let j=1;j<=5;j++){
+//         console.log(i,j);
+        
+
+//     }
+    
+// }
+
+// let num=5;
+// for(let i=1;i<=num;i++){
+//     let stars='';
+//     for(let j=1;j<=num;j++){
+//         stars=stars + j;
+        
+//     }
+//     console.log(stars)
+// }
+
+// let num=5;
+// for(let i=1;i<=num;i++){
+//     let stars='';
+//     for(let j=1;j<=i;j++){
+//         stars=stars + '*';
+        
+
+//     }
+//     console.log(stars);
+// }
