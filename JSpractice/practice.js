84b8ec9fbd5d39960a4680c5025d3765fea79ad5
Chandan Kumar <krc6893@gmail.com>
2022-06-30 14:29:01 +0530

    Changes

--- JSpractice/practice.js
+++ JSpractice/practice.js
@@ -53,7 +53,7 @@
 // for(let i=1;i<=num;i++){
 //     let stars='';
 //     for(let j=1;j<=num;j++){
-//         stars=stars + j;
+//         stars=stars + i;
         
 //     }
 //     console.log(stars)
@@ -69,3 +69,71 @@
 //     }
 //     console.log(stars);
 // }
+
+
+
+// let num=5;
+// let count=1;
+
+// for(let i=1;i<=num;i++){
+//     let sum='';
+//     for(let j=1;j<=i;j++){
+//         sum=sum+`${count}  `;
+//         count=count+1;
+        
+//     }
+//     console.log(sum);
+// }
+
+
+// let num=5;
+// for(let i=1;i<=num;i++){
+//     let space='';
+//     let count;
+    
+//     for(let j=1;j<=i;j++){ 
+//         if(i%2==0){
+//             count=0;
+//         }
+//         else{
+//             count=1;
+//         }
+//         space=space+count;
+
+//     }
+//     console.log(space);
+// }
+
+// let num=5;
+// for(let i=1;i<=num;i++){
+//     let space='';
+//     let count;
+    
+//     for(let j=1;j<=i;j++){ 
+//         if(j%2==0){
+//             count=0;
+//         }
+//         else{
+//             count=1;
+//         }
+//         space=space+count;
+
+//     }
+//     console.log(space);
+// }
+
+let num=6;
+for(let i=1;i<=num;i++){
+    space='';
+    for(let j=1;j<=i;j++){
+        if((i+j)%2==0){
+            space=space+1;
+        }
+        else{
+            space=space+0;
+        }
+    }
+    console.log(space);
+}
+
+
