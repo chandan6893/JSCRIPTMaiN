2da6fd360e07aaee21d6f01548b6f4b7a62e6532
Chandan Kumar <krc6893@gmail.com>
2022-07-02 14:03:58 +0530

    Practiced pattern

---
+++ 02July2022/practice.js
@@ -0,0 +1,21 @@
+// let num=5;
+// for(let i=num;i>=1;i--){
+//     let space='';
+//     for(let j=1;j<=i;j++){
+//         space=space+'*';
+//         // console.log(space)
+//     }
+//     console.log(space)
+// }
+
+// for(let i=1;i<=3;i++){
+//     let space='';
+//     for(let j=2*i-1;j<5;j++){
+//         space=space+j
+//     }
+//     console.log(space)
+// }
+function add(a,b){
+    return a+b
+}
+console.log(add(5,4))
