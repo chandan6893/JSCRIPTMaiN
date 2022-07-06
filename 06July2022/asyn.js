a39727e665c10dc059814eae2db7e2e13ef65796
Chandan Kumar <krc6893@gmail.com>
2022-07-06 17:21:35 +0530

    Callback

---
+++ 06July2022/asyn.js
@@ -0,0 +1,12 @@
+// ASYNCHRONOUS JSON
+
+// console.log("I");
+
+// // This will be shown after 2 seconds
+
+// setTimeout(()=>{
+//   console.log("eat");
+// },2000)
+
+// console.log("Ice Cream")
+
---
+++ 06July2022/readme.md
@@ -0,0 +1,1 @@
+<img src="Im\Icecream.png" alt="">
