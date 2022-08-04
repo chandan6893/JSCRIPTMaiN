1b756222ed8502fb37af297fac6127bef0bf2c21
Chandan Kumar <krc6893@gmail.com>
2022-08-04 21:36:56 +0530

    CALCULATOR

---
+++ August2022/04Aug2022/calc/index.js
@@ -0,0 +1,25 @@
+let display=document.getElementById("display");
+
+
+let buttons=Array.from(document.getElementsByClassName("btn"));
+// console.log(buttons)
+buttons.map((btn)=>{
+    btn.addEventListener("click",(event)=>{
+        let key=event.target.innerText;
+        if(key == "A/c"){
+            display.innerText = "";
+        }
+        else if(key=="del"){
+            display.innerText=display.innerText.slice(0,-1);
+        }
+        else if(key== "="){
+            if(display.innerText){
+                display.innerText=eval(display.innerText);
+            }
+        }
+        else{
+            // display.innerText=display.innerText+key;
+            display.innerText+=key;
+        }
+    });
+});
