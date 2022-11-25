2210030df95296e3f74548630d484026e3963d42
Chandan Kumar <krc6893@gmail.com>
2022-11-25 15:59:54 +0530

    querySelector,querySelectorAll,toggle,attribute

--- Nov2022/25Nov2022/querySelector.js/qSelector.js
+++ Nov2022/25Nov2022/querySelector.js/qSelector.js
@@ -9,4 +9,17 @@ parentDiv.style.color="darkgreen";
 
 
 let list = document.querySelectorAll("ul li");
-console.log(list)
+console.log(list);
+
+document.querySelector("a").classList.add("anchor");
+// document.querySelector("a").classList.remove("anchor");
+document.querySelector('a').classList.toggle("anchor");
+
+
+
+// document.querySelector("div.container").innerText="HELLO";
+// document.querySelector("div para").innerText="para";
+
+function toggle(){
+    document.querySelector("a").classList.toggle("anchor");
+}
