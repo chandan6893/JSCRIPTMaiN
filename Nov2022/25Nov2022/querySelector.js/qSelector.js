a67f0e3b3bb488089f60275b9845bc637db912fb
Chandan Kumar <krc6893@gmail.com>
2022-11-25 14:30:57 +0530

    querySelector and querySelectorAll

---
+++ Nov2022/25Nov2022/querySelector.js/qSelector.js
@@ -0,0 +1,12 @@
+const parentDiv = document.querySelector("div");
+parentDiv.innerText="hello querySelector";
+parentDiv.style.fontSize="8rem";
+parentDiv.style.color="darkgreen";
+// console.log(parentDiv);
+
+// const heading1 = document.createElement("h1");
+// console.log(heading1);
+
+
+let list = document.querySelectorAll("ul li");
+console.log(list)
