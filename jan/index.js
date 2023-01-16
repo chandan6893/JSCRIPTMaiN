4cfa8531ed2cf48ef5f9d348dae7fb33ffd3ebf3
Chandan Kumar <krc6893@gmail.com>
2023-01-16 14:38:03 +0530

    custom map and filetr method

--- jan/index.js
+++ jan/index.js
@@ -1,3 +1,5 @@
+// ***Custom map Method
+
 function myArr(arr, cb) {
   let newArr = [];
   for (let i = 0; i < arr.length; i++) {
@@ -11,3 +13,23 @@ function square(x) {
   return x * x;
 }
 console.log(myArr([1, 2, 3, 4], square));
+
+
+//Custom Filter Method
+function myFilter(arr, cb) {
+  let filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i])) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
+}
+
+function isEven(x) {
+  if (x % 2 === 0) {
+    return x;
+  }
+}
+
+console.log(myFilter([2, 3, 4, 5, 7], isEven));
