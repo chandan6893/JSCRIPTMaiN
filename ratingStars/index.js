a4194a74e63d1727ba4be500494ebafac0c5cf8c
Chandan Kumar <krc6893@gmail.com>
2023-03-08 10:42:31 +0530

    stars Rating

--- ratingStars/index.js
+++ ratingStars/index.js
@@ -1,13 +1,15 @@
 const stars = document.querySelectorAll(".fa-solid");
-// console.log(stars);
+console.log(stars);
 
 stars.forEach((item,index1)=>{
-    // console.log(index1)
+    console.log(index1)
     item.addEventListener("click",()=>{
         stars.forEach((ele,index2)=>{
-            // console.log(index2)
-            index1>=index2 ? ele.classList.add("yell") : ele.classList.remove("yell")
+            console.log(index2)
+            index1 >=index2 ? ele.classList.add("yell") : ele.classList.remove("yell")
         })
     })
 })
+
+
 
