a0de6cc669fd59e0d3a96bac83cdbbaa09ff5d65
Chandan Kumar <krc6893@gmail.com>
2023-04-05 10:07:17 +0530

    fibbonaci

--- JSpractice/practice.js
+++ JSpractice/practice.js
@@ -353,13 +353,25 @@ const person = {
 // }
 
 // fibbonaci
-let a = 0;
-let b = 1;
-console.log(a);
-console.log(b);
-for (let i = 0; i < 10; i++) {
-  let temp = a + b;
-  console.log(temp);
-  a = b;
-  b = temp;
+// let a = 0;
+// let b = 1;
+// console.log(a);
+// console.log(b);
+// for (let i = 0; i < 10; i++) {
+//   let temp = a + b;
+//   console.log(temp);
+//   a = b;
+//   b = temp;
+// }
+
+const num=100;
+let x=0;
+let y=1;
+let fn=x+y;
+console.log(x);
+while(fn<num){
+console.log(fn)
+fn=x+y;
+x=y;
+y=fn;
 }
