b5b3f62190c234864e0bdf7471c8e73ca3d098aa
Chandan Kumar <krc6893@gmail.com>
2023-05-07 15:01:52 +0530

    pollyfill for forEach,map,filter

--- Polyfills/app.js
+++ Polyfills/app.js
@@ -23,3 +23,50 @@ Array.prototype.myMap = function (cb) {
   return newArr;
 };
 console.log(arr.myMap((ele) => ele * 3));
+
+// polyfill for map
+
+// let arr = [1, 2, 3, 12];
+
+// let newArr=arr.map((ele)=>{
+//   return ele;
+// });
+// console.log(newArr);
+
+
+function squareOfTheEle(ele) {
+  return ele % 2 === 0;
+}
+
+Array.prototype.myMap = function (cb) {
+  // console.log("This",this)
+  let newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i])) {
+      newArr.push(this[i]);
+    }
+  }
+  return newArr;
+};
+console.log(arr.myMap(squareOfTheEle));
+
+
+// polyfill for filter
+
+// const arr = [1, 2, 3, 4, 5];
+// const result=arr.filter((el)=>el>2);
+// console.log(result);
+
+Array.prototype.myFilter = function (cb) {
+  // console.log(this)
+  let newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i])) {
+      newArr.push(this[i]);
+    }
+  }
+  return newArr;
+};
+
+const result = arr.myFilter((el) => el > 2);
+console.log(result);
